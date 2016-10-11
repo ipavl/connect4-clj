74fa5934ff6f48856d445829a7ea1204819e23d0
Ian Pavlinic <ipavlini@mail.uoguelph.ca>
2016-10-10 23:15:40 -0400

    Send messages from IRC to the web interface

--- src/clj/om_sente/gameserver.clj
+++ src/clj/om_sente/gameserver.clj
@@ -1,9 +1,16 @@
 (ns om-sente.gameserver
-  (:require [irclj.core :as i]))
+  (:require [irclj.core :as i]
+            [irclj.parser :as p]
+            [clojure.string :as str]
+            [om-sente.websocket :as ws]))
 
 (defn callback
+  "Handles PRIVMSG responses from IRC. Sends the message
+   down the websocket for the web interface to handle."
   [irc type & s]
-  (prn irc))
+  ;; Hacky way to get the user's uid back from their nick
+  (let [uid (last (str/split (irc :nick) #"-"))]
+    (ws/chsk-send! (Integer/parseInt uid) [:test/reply (type :text)])))
 
 (defn connect
   "Connects to the given IRC server."
