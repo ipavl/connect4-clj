85a4ea5a558bfdaaf3616c2d1b5d0b97f8ba8b7b
Ian Pavlinic <ipavlini@mail.uoguelph.ca>
2016-12-08 01:30:15 -0500

    Handle IndexOutOfBoundsException when a column is full

--- src/clj/connect4/command_handler_helpers.clj
+++ src/clj/connect4/command_handler_helpers.clj
@@ -12,12 +12,15 @@
    Uses a bottom-up approach."
   [board col]
   (loop [x (- p/board-height 1)]
-    (if (nil? (get-in board [x col]))
-      x
-      (recur (dec x)))))
+    (if (<= 0 x)
+      (if (nil? (get-in board [x col]))
+        x
+        (recur (dec x))))))
 
 (defn create-updated-board
-  "Given a board and a column to add to, creates a new board."
+  "Given a board and a column to add to, creates a new board. If the given
+   column is at its maximum height, returns the old board."
   [board col]
-  (let [row (get-next-available-row board col)]
-    (assoc-in board [row col] "X")))
+  (if-let [row (get-next-available-row board col)]
+    (assoc-in board [row col] "X")
+    board))
