df63d82d20b178a01fe52893e1e468c5638e4539
Ian Pavlinic <ipavlini@mail.uoguelph.ca>
2016-12-08 01:14:44 -0500

    Dynamically calculate the column height based on the board

--- src/clj/connect4/command_handler_helpers.clj
+++ src/clj/connect4/command_handler_helpers.clj
@@ -8,12 +8,16 @@
   (vec (repeat p/board-height (vec (repeat p/board-width nil)))))
 
 (defn get-next-available-row
-  "Loops over the rows to find the lowest available square for a given column."
-  [col]
-  5)
+  "Loops over the rows to find the lowest available square for a given column.
+   Uses a bottom-up approach."
+  [board col]
+  (loop [x (- p/board-height 1)]
+    (if (nil? (get-in board [x col]))
+      x
+      (recur (dec x)))))
 
 (defn create-updated-board
   "Given a board and a column to add to, creates a new board."
   [board col]
-  (let [row 5]
+  (let [row (get-next-available-row board col)]
     (assoc-in board [row col] "X")))
